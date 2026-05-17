b2be40c973bd5c6093620d093922104a046a6dd4
kayoung-dev <kayoung150@gmail.com>
2026-05-17 22:02:40 +0900

    feat: add document type filter

---
+++ src/config/documentTypes.js
@@ -0,0 +1,16 @@
+export const DOCUMENT_TYPE_FILTER_ALL = "all";
+
+export const DOCUMENT_TYPES = [
+    {
+        id: "worklog",
+        label: "TA 근무일지",
+        requestNameMarker: null,
+    },
+    {
+        id: "research",
+        label: "연구참여확약서",
+        requestNameMarker: "[연구참여확약서]",
+    },
+];
+
+export const DOCUMENT_TYPE_FILTER_LABEL = "문서종류";
---
+++ src/utils/documentTypeUtils.js
@@ -0,0 +1,23 @@
+import {
+    DOCUMENT_TYPE_FILTER_ALL,
+    DOCUMENT_TYPES,
+} from "../config/documentTypes";
+
+const researchType = DOCUMENT_TYPES.find((type) => type.id === "research");
+
+export const isResearchDocument = (requestName = "") =>
+    Boolean(researchType?.requestNameMarker) &&
+    requestName.includes(researchType.requestNameMarker);
+
+export const getDocumentTypeId = (requestName = "") =>
+    isResearchDocument(requestName) ? "research" : "worklog";
+
+export const matchesDocumentTypeFilter = (requestName, filterId) => {
+    if (filterId === DOCUMENT_TYPE_FILTER_ALL) return true;
+    return getDocumentTypeId(requestName) === filterId;
+};
+
+export const getDocumentTypeFilterOptions = () => [
+    { value: DOCUMENT_TYPE_FILTER_ALL, label: "문서종류" },
+    ...DOCUMENT_TYPES.map(({ id, label }) => ({ value: id, label })),
+];
